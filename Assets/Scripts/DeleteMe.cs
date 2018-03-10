7c94d2e4f087ca363b4f9f86037ea5297691e0a1
Raphael Chriqui <raphael.chriqui@epitech.eu>
2018-03-10 14:33:39 +0100

    Init finished scoring display

---
+++ Assets/Scripts/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteMe : MonoBehaviour {
+    public float delay;
+	void Start () {
+	}
+	
+	void Update () {
+        delay -= Time.deltaTime;
+        if (delay <= 0)
+            Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float plane_speed = 10;
+    public float projetile_speed = 5000000000;
+
+    void Start () {
+	}
+	
+	void Update () {
+        transform.position += transform.forward * Time.deltaTime * plane_speed;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, 50 * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject toInstantiate = Resources.Load<GameObject>("Projectile");
+            GameObject projectile = Instantiate(toInstantiate, transform.position, Quaternion.identity);
+            projectile.name = "Projectile";
+            projectile.GetComponent<Rigidbody>().AddForce(transform.forward * projetile_speed, ForceMode.Impulse);
+        }
+    }
+}
